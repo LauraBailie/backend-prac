6db18380a57ad2284549e996dcc5f42b6d813896
Laura Bailie <laurabailie11@gmail.com>
2026-02-24 17:59:29 +0200

    feat: add auth APIs (register, login, logout)

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,90 @@
+import { User } from "../models/user.model.js";
+
+const registerUser = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+
+        // basic validation
+
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: "All fields are required."})
+        }
+
+        // check if user already exists
+
+        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "Username or email already exists." });
+        }
+
+        // create new user
+
+        const user = await User.create({
+            username,
+            email,
+            password,
+        });
+
+        return res.status(201).json({
+            message: "User registered successfully.",
+            user: { id: user._id, username: user.username, email: user.email },
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error.", error: error.message });
+    }
+};
+
+const loginUser = async (req, res) => {
+    try {
+        
+        // check if user already exists
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email: email});
+
+        if (!user) return res.status(400).json({ message: "User not found." });
+        
+
+        // compare passwords
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) return res.status(400).json({ 
+            message: "Invalid email or password." 
+        })
+
+        res.status(200).json({
+            message: "User logged in successfully.",
+            user: { 
+                id: user._id, 
+                username: user.username, 
+                email: user.email 
+            }
+        })
+    } catch (error) {
+        res.status(500).json({ 
+            message: "Server error.", error: error.message 
+        })
+    }
+}
+
+const logoutUser = async (req, res) => {
+    try {
+        const { email } = req.body;
+
+        const user = await User.findOne({ email: email});
+
+        if(!user) return res.status(404).json({ message: "User not found." });
+
+        res.status(200).json({ message: "Logout successful."});
+    } catch (error) {
+        res.status(500).json({ message: "Server error.", error: error.message 
+        });
+        
+    }
+}
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser,
+}
